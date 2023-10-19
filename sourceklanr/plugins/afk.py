43b5eaa88f33670935e4946eef85d53f7ef9cbd4
telethonArab <105546676+telethonArab@users.noreply.github.com>
2023-10-19 14:43:37 +0300

    Update afk.py

--- sourceklanr/plugins/afk.py
+++ sourceklanr/plugins/afk.py
@@ -1,7 +1,7 @@
 import asyncio
 from datetime import datetime
 from telethon.tl import functions, types
-from IqArab import iqthon
+from sourceklanr import iqthon
 from ..Config import Config
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
