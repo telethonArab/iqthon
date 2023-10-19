5faeb92b6885dcc6bd2b02b4bfd31157e8b768b9
telethonArab <105546676+telethonArab@users.noreply.github.com>
2023-10-19 14:47:54 +0300

    Update nw.py

--- sourceklanr/plugins/nw.py
+++ sourceklanr/plugins/nw.py
@@ -1,6 +1,6 @@
 from ..utils import is_admin
 from googletrans import Translator
-from IqArab import iqthon
+from sourceklanr import iqthon
 from telethon.sync import functions
 from telethon import events
 from ..sql_helper.globals import addgvar, delgvar, gvarstatus
